27c70f3df9638c069d0c83ba70c58bdf27d8c0ff
Denys Dronov <dronovdenis36@gmail.com>
2023-11-06 10:41:28 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp11/FunctionCache.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class FunctionCache<TKey, TResult>
+    {
+        private readonly Dictionary<TKey, CacheItem> cache = new Dictionary<TKey, CacheItem>();
+        private readonly TimeSpan cacheDuration;
+
+        public delegate TResult Func<TKey, TResult>(TKey key);
+
+        public FunctionCache(TimeSpan cacheDuration)
+        {
+            this.cacheDuration = cacheDuration;
+        }
+
+        public TResult GetOrAdd(TKey key, Func<TKey, TResult> func)
+        {
+            if (cache.TryGetValue(key, out var cacheItem) && !IsCacheItemExpired(cacheItem))
+            {
+                return cacheItem.Value;
+            }
+
+            TResult result = func(key);
+            AddOrUpdateCacheItem(key, result);
+            return result;
+        }
+
+        private bool IsCacheItemExpired(CacheItem cacheItem)
+        {
+            return DateTime.Now - cacheItem.Timestamp > cacheDuration;
+        }
+
+        private void AddOrUpdateCacheItem(TKey key, TResult result)
+        {
+            if (cache.ContainsKey(key))
+            {
+                cache[key] = new CacheItem(result);
+            }
+            else
+            {
+                cache.Add(key, new CacheItem(result));
+            }
+        }
+
+        internal int GetOrAdd(string key1, System.Func<string, int> expensiveFunction)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class CacheItem
+        {
+            public CacheItem(TResult value)
+            {
+                Value = value;
+                Timestamp = DateTime.Now;
+            }
+
+            public TResult Value { get; }
+            public DateTime Timestamp { get; }
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp11;
+
+static void Main()
+{
+    FunctionCache<string, int> cache = new FunctionCache<string, int>(TimeSpan.FromMinutes(10));
+
+    Func<string, int> expensiveFunction = (key) =>
+    {
+        Console.WriteLine($"Executing expensive function for key: {key}");
+        return key.Length;
+    };
+
+    string key1 = "abc";
+    string key2 = "def";
+
+    int result1 = cache.GetOrAdd(key1, expensiveFunction);
+    int result2 = cache.GetOrAdd(key1, expensiveFunction); 
+
+    int result3 = cache.GetOrAdd(key2, expensiveFunction); 
+
+    Console.WriteLine($"Result 1: {result1}");
+    Console.WriteLine($"Result 2 (from cache): {result2}");
+    Console.WriteLine($"Result 3: {result3}");
+}
